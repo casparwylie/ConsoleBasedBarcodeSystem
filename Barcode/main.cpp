a6d7f8c5a02d9e3d2649d5853038aeb3b6b08e50
Caspar Wylie <casparwylie@gmail.com>
2016-07-25 19:03:53 +0100

    Initial Commit

---
+++ Barcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Barcode
+//
+//  Created by Caspar Wylie on 25/07/16.
+//  Copyright (c) 2016 Caspar Wylie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
